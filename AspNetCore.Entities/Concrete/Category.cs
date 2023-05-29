5cfdf742ffd8a46ef6ca67d8590990193d488c89
Mert Özen <mrtozn1923@gmail.com>
2023-05-29 23:00:45 +0300

    article category comment role user object created

---
+++ AspNetCore.Entities/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using AspNetCoreBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Entities.Concrete
+{
+    public class Category:EntityBase,IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; }  
+    }
+}
