f84f2e481ac156e263f43ec577e23802b7132336
Mert Özen <mrtozn1923@gmail.com>
2023-06-24 17:09:57 +0300

    unitofwork completed

---
+++ AspNetCore.Data/Concrete/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using AspNetCoreBlog.Data.Abstract;
+using AspNetCoreBlog.Data.Concrete.EntityFramework.Contexts;
+using AspNetCoreBlog.Data.Concrete.EntityFramework.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreBlog.Data.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AspNetCoreBlogContext _context;
+        private EfArticleRepository _articleRepository;
+        private EfCategoryRepository _categoryRepository;
+        private EfCommentRepository _commentRepository;
+        private EfRoleRepository _roleRepository;
+        private EfUserRepository _userRepository;
+
+        public UnitOfWork(AspNetCoreBlogContext context)
+        {
+            _context = context;
+        }
+
+        public IArticleRepository Articles => _articleRepository ?? new EfArticleRepository(_context);
+        public ICategoryRepository Categories => _categoryRepository ?? new EfCategoryRepository(_context);
+
+        public ICommentRepository Comments => _commentRepository ?? new EfCommentRepository(_context);
+
+        public IRoleRepository Roles => _roleRepository ?? new EfRoleRepository(_context);
+
+        public IUserRepository Users => _userRepository ?? new EfUserRepository(_context);
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+    }
+}
