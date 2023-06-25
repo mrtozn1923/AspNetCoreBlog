67dd3d4f3b14d6fe33360c93ace61bf6662c6469
Mert Özen <mrtozn1923@gmail.com>
2023-06-25 18:18:33 +0300

    article dtos completed

--- AspNetCore.Entities/Dtos/ArticleDto.cs
+++ AspNetCore.Entities/Dtos/ArticleDto.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AspNetCore.Entities.Concrete;
+using AspNetCoreBlog.Shared.Entities.Abstract;
+using AspNetCoreBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,8 @@ using System.Threading.Tasks;
 
 namespace AspNetCoreBlog.Entities.Dtos
 {
-    public class ArticleDto
+    public class ArticleDto:DtoGetBase
     {
+        public Article Article { get; set; }
     }
 }
