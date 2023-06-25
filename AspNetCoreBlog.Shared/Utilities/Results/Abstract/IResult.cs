7115960b9a76485c1200a9ac01b95964463cc959
Mert Özen <mrtozn1923@gmail.com>
2023-06-25 16:49:14 +0300

    result structure created

---
+++ AspNetCoreBlog.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,16 @@
+﻿using AspNetCoreBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreBlog.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }  
+        public Exception Exception { get; }
+    }
+}
